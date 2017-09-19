a691e91ef847b9847d423dbe002c676e3c88be59
Egila Karen <Egila Karen@192.168.1.7>
2017-09-19 16:30:29 -0300

    Slide 3

--- CursoJPA/src/main/java/br/edu/uni7/persistence/Documento.java
+++ CursoJPA/src/main/java/br/edu/uni7/persistence/Documento.java
@@ -47,4 +47,29 @@ public class Documento {
 	public void setVersao(Long versao) {
 		this.versao = versao;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Documento other = (Documento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
